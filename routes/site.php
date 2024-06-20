9025144642bffebf89ea7184bc1326172b56580f
Claudio Perroni <c.perroni@ifortech.com>
2024-06-20 08:26:42 +0200

    modifiche 20 giugno inizio

--- routes/site.php
+++ routes/site.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    //return view('welcome');
-    return view('homepage');
+    return view('welcome');
+    //return view('homepage');
 });
 
 Route::get('/academies-map', function () {
