911eb167df29e11a450af22cf827bee706417729
Claudio Perroni <c.perroni@ifortech.com>
2024-09-25 12:06:43 +0200

    e

--- app/Listeners/SendFeePaidEmail.php
+++ app/Listeners/SendFeePaidEmail.php
@@ -6,21 +6,20 @@ use App\Events\FeePaid;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendFeePaidEmail
-{
+class SendFeePaidEmail {
     /**
      * Create the event listener.
      */
-    public function __construct()
-    {
+    public function __construct() {
         //
     }
 
     /**
      * Handle the event.
      */
-    public function handle(FeePaid $event): void
-    {
+    public function handle(FeePaid $event): void {
         //
+
+        //$order = $event->order;
     }
 }
