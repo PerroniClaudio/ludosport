0cd7a8713379c72c973ffe9d9f412cf8ea87d136
Claudio Perroni <c.perroni@ifortech.com>
2024-10-23 10:09:49 +0200

    tesserino

---
+++ resources/views/components/user/id-card.blade.php
@@ -0,0 +1,49 @@
+@props([
+    'user' => null,
+])
+
+<section class="w-full bg-background-900 border-8 border-primary-500 flex flex-col p-4 gap-4">
+    <div class="grid grid-cols-4 items-center w-full  gap-4">
+        <div class="flex items-center gap-1">
+            <div class="flex-[2_1_0]">
+                <p class="lg:text-6xl text-primary-500 uppercase font-bold">{{ $user->nation->name }}</p>
+            </div>
+            <div class="flex-[1_1_0]">
+                <img src="{{ route('nation-flag', $user->nation->id) }}" alt="{{ $user->nation->flag }}">
+            </div>
+        </div>
+        <div class="col-span-2 flex flex-col">
+            <p class="text-white font-bold lg:text-3xl uppercase">{{ $user->name }} {{ $user->surname }}</p>
+            <p class="text-primary-500 font-bold lg:text-3xl">
+                {{ __('users.since_to', [
+                    'since' => $user->subscription_year,
+                    'to' => now()->format('Y'),
+                ]) }}
+            </p>
+        </div>
+        <div class="flex justify-end">
+            <p class="text-xs lg:text-lg">
+                <span class="text-primary-500">
+                    {{ __('users.uninque_id_card') }}
+                </span>
+                <span class="text-white">
+                    {{ $user->unique_code }}
+                </span>
+            </p>
+        </div>
+    </div>
+    <div class="flex items-center gap-2">
+        <p class="text-white text-3xl">{{ $user->battle_name }}</p>
+
+        @if ($user->has_paid_fee)
+            <x-lucide-circle class="h-6 w-6 text-green-500" />
+        @endif
+    </div>
+    <div>
+        <p class="text-white font-bold">LudoSport</p>
+        <p class="font-bold">
+            <span class="text-primary-500 ">{{ __('school.school') }}:</span>
+            <span class="text-white">{{ $user->primarySchoolAthlete()->name }}</span>
+        </p>
+    </div>
+</section>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <x-user.id-card :user="$user" />
+
             <div class="p-4 sm:p-8 bg-white dark:bg-background-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
