7ebc049c4fb357db9508da9add1ed25e0207ea00
erasmosalsanoifortech <e.salsano@ifortech.com>
2024-12-24 11:34:13 +0100

    aggiunto no roles nella lista utenti dell'admin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -146,9 +146,18 @@ class UserController extends Controller {
             }
         }
 
+        // put all users without roles in a no-role array inside the users_sorted_by_role array
+        $no_roles_users = $users->filter(function ($user) {
+            return $user->roles->isEmpty();
+        });
+        $users_without_roles = [];
+        foreach ($no_roles_users as $user) {
+            $users_without_roles[] = $user;
+        }
 
         return view($viewPath, [
             'users' => $users_sorted_by_role,
+            'users_without_roles' => $users_without_roles,
             'roles' => $roles,
         ]);
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -36,11 +36,68 @@
 
                                     </button>
                                 @endforeach
+                                <button
+                                    class="hover:text-background-800 dark:hover:text-background-300 focus:outline-none focus:text-primary-600 dark:focus:text-primary-600 text-left"
+                                    :class="{ 'text-primary-600': selectedRole == 'no_role' }"
+                                    @click="selectedRole = 'no_role'">
+
+                                    {{ __('users.no_roles') }}
+
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-span-12 md:col-span-9">
+
+                    <template x-if="selectedRole == 'no_role'">
+                        <div class="bg-white dark:bg-background-800 overflow-hidden shadow-sm sm:rounded-lg"
+                                x-show="selectedRole == 'no_role'" x-cloak>
+                                <div class="p-6 text-background-900 dark:text-background-100">
+                                    <h3 class="text-background-800 dark:text-background-200 text-2xl">
+                                        {{ __('users.no_role') }}
+                                    </h3>
+                                    <div class="border-b border-background-100 dark:border-background-700 my-2"></div>
+                                    <x-table striped="false" :columns="[
+                                        [
+                                            'name' => 'ID',
+                                            'field' => 'id',
+                                            'columnClasses' => '',
+                                            'rowClasses' => '',
+                                        ],
+                                        [
+                                            'name' => 'Name',
+                                            'field' => 'name',
+                                            'columnClasses' => '',
+                                            'rowClasses' => '',
+                                        ],
+                                        [
+                                            'name' => 'Email',
+                                            'field' => 'email',
+                                            'columnClasses' => '',
+                                            'rowClasses' => '',
+                                        ],
+                                    ]" :rows="$users_without_roles">
+                                        <x-slot name="tableRows">
+                                            <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                                x-text="row.id"></td>
+                                            <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                                x-text="row.name + ' ' + row.surname"></td>
+                                            <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                                x-text="row.email"></td>
+                                            <td
+                                                class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap">
+                                                <a x-bind:href="'/users/' + row.id">
+                                                    <x-lucide-pencil
+                                                        class="w-5 h-5 text-primary-800 dark:text-primary-500 cursor-pointer" />
+                                                </a>
+                                            </td>
+                                        </x-slot>
+                                    </x-table>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
 
                     @foreach ($users as $key => $role)
                         <div class="bg-white dark:bg-background-800 overflow-hidden shadow-sm sm:rounded-lg"
