4215b9546e78614b763fde4fe493aeda69c701c6
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-01-17 10:00:09 +0100

    fix forme uguali tra instructor e technician in lista utenti

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -217,7 +217,7 @@
                                     </x-slot>
 
                                 </x-table>
-                            @elseif ($key == 'instructor' || $key == 'technician')
+                            @elseif ($key == 'instructor')
                                 <h3 class="text-background-800 dark:text-background-200 text-2xl">
                                     {{ __('users.' . $key . '_role') }}
                                 </h3>
@@ -243,7 +243,7 @@
                                     ],
                                     [
                                         'name' => 'Weapon Forms',
-                                        'field' => 'weapon_forms_formatted',
+                                        'field' => 'weapon_forms_instructor_formatted',
                                         'columnClasses' => '',
                                         'rowClasses' => '',
                                     ],
@@ -256,7 +256,60 @@
                                         <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
                                             x-text="row.email"></td>
                                         <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
-                                            x-text="row.weapon_forms_formatted"></td>
+                                            x-text="row.weapon_forms_instructor_formatted"></td>
+                                        <td
+                                            class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap">
+                                            <a x-bind:href="'/users/' + row.id">
+                                                <x-lucide-pencil
+                                                    class="w-5 h-5 text-primary-800 dark:text-primary-500 cursor-pointer" />
+                                            </a>
+                                        </td>
+                                    </x-slot>
+
+                                    <x-slot name="tableActions">
+
+                                    </x-slot>
+                                </x-table>
+                            @elseif ($key == 'technician')
+                                <h3 class="text-background-800 dark:text-background-200 text-2xl">
+                                    {{ __('users.' . $key . '_role') }}
+                                </h3>
+                                <div class="border-b border-background-100 dark:border-background-700 my-2"></div>
+                                <x-table striped="false" :columns="[
+                                    [
+                                        'name' => 'ID',
+                                        'field' => 'id',
+                                        'columnClasses' => '',
+                                        'rowClasses' => '',
+                                    ],
+                                    [
+                                        'name' => 'Name',
+                                        'field' => 'name',
+                                        'columnClasses' => '',
+                                        'rowClasses' => '',
+                                    ],
+                                    [
+                                        'name' => 'Email',
+                                        'field' => 'email',
+                                        'columnClasses' => '',
+                                        'rowClasses' => '',
+                                    ],
+                                    [
+                                        'name' => 'Weapon Forms',
+                                        'field' => 'weapon_forms_technician_formatted',
+                                        'columnClasses' => '',
+                                        'rowClasses' => '',
+                                    ],
+                                ]" :rows="$role">
+                                    <x-slot name="tableRows">
+                                        <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                            x-text="row.id"></td>
+                                        <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                            x-text="row.name + ' ' + row.surname"></td>
+                                        <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                            x-text="row.email"></td>
+                                        <td class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap"
+                                            x-text="row.weapon_forms_technician_formatted"></td>
                                         <td
                                             class="text-background-500 dark:text-background-300 px-6 py-3 border-t border-background-100 dark:border-background-700 whitespace-nowrap">
                                             <a x-bind:href="'/users/' + row.id">
