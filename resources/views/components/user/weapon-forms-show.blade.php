5f27494311eac61636b28baba977be26d3476207
Claudio Perroni <c.perroni@ifortech.com>
2025-01-21 14:52:50 +0100

    weapon form sul profilo utente

--- resources/views/components/user/weapon-forms-show.blade.php
+++ resources/views/components/user/weapon-forms-show.blade.php
@@ -1,17 +1,8 @@
 @props([
     'forms' => [],
+    'user' => [],
 ])
 
-@php
-    use App\Models\WeaponForm;
-    $weapon_forms = WeaponForm::all();
-
-    $weapon_forms_cycle2 = $weapon_forms->skip(3);
-    $weapon_forms_cycle2 = $weapon_forms_cycle2->sortBy('name');
-
-    $user_forms = $forms->pluck('id')->toArray();
-
-@endphp
 
 <section
     class="bg-white dark:bg-background-800 overflow-hidden shadow-sm sm:rounded-lg p-8 text-background-800 dark:text-background-200">
@@ -19,41 +10,107 @@
     <div class="border-b border-background-100 dark:border-background-700 my-2"></div>
 
     <div class="grid grid-cols-3 gap-2 mb-2">
+        <div class="flex flex-col items-center justify-center">
+            <img src="{{ route('weapon-image', [
+                'weapon' => 'longsaber',
+            ]) }}"
+                alt="long saber" class="w-12 h-12 invert">
+        </div>
+        <div class="flex flex-col items-center justify-center">
+            <img src="{{ route('weapon-image', [
+                'weapon' => 'saberstaff',
+            ]) }}"
+                alt="long saber" class="w-12 h-12 invert">
+        </div>
+        <div class="flex flex-col items-center justify-center">
+            <img src="{{ route('weapon-image', [
+                'weapon' => 'dualsaber',
+            ]) }}"
+                alt="long saber" class="w-12 h-12 invert">
+        </div>
+
+    </div>
+
+    @php
+        use App\Models\WeaponForm;
+        $weapon_forms = WeaponForm::all();
+
+        $weapon_forms_cycle2 = $weapon_forms->skip(3);
+        $weapon_forms_cycle2 = $weapon_forms_cycle2->sortBy('name');
+
+        $long_saber_weapon_forms = [];
+        $saberstaff_weapon_forms = [];
+        $dual_saber_weapon_forms = [];
+
+        foreach ($weapon_forms_cycle2 as $weapon_form) {
+            if (strpos($weapon_form->name, 'Long Saber') !== false) {
+                $long_saber_weapon_forms[] = $weapon_form;
+            }
+
+            if (strpos($weapon_form->name, 'Saberstaff') !== false) {
+                $saberstaff_weapon_forms[] = $weapon_form;
+            }
+
+            if (strpos($weapon_form->name, 'Dual Saber') !== false) {
+                $dual_saber_weapon_forms[] = $weapon_form;
+            }
+        }
+
+    @endphp
+
+
+    <div class="grid grid-cols-3 gap-2 mb-2">
 
         @foreach ($weapon_forms->take(3) as $weapon_form)
-            <div
-                class="flex items-center justify-center col-span-3 {{ in_array($weapon_form->id, $user_forms) ? '' : 'opacity-30' }}">
-                <div class="bg-primary-500 p-2 rounded">
-                    <img src="{{ route('weapon-form-image', $weapon_form->id) }}" alt="{{ $weapon_form->name }}"
-                        class="w-12 h-12">
-                </div>
+            <div class="flex items-center justify-center col-span-3  ">
+
+                <img src="{{ route('weapon-form-image-user', [
+                    'weapon' => $weapon_form->id,
+                    'user' => $user->id,
+                ]) }}"
+                    alt="{{ $weapon_form->name }}" class="w-8 h-8" style="">
+
             </div>
         @endforeach
 
     </div>
 
-    <div class="grid grid-cols-3 rounded">
+    <div class="grid grid-cols-3 rounded gap-2 mb-2">
 
-        <div class="flex items-center justify-center text-primary-500 bg-background-900 rounded-l">
-            <p>Dual Saber</p>
-        </div>
-        <div class="flex items-center justify-center text-primary-500 bg-background-900">
-            <p>Long Saber</p>
-        </div>
-        <div class="flex items-center justify-center text-primary-500 bg-background-900 rounded-r">
-            <p>Saberstaff</p>
-        </div>
-
-        @foreach ($weapon_forms_cycle2 as $weapon_form)
-            <div class="my-2 mx-2  {{ in_array($weapon_form->id, $user_forms) ? '' : 'opacity-30' }}">
-                <div
-                    class="{{ strpos($weapon_form->name, '6') !== false || strpos($weapon_form->name, '7') !== false ? 'hidden' : 'bg-primary-500' }}  p-2 rounded w-full flex items-center justify-center ">
-                    <img src="{{ route('weapon-form-image', $weapon_form->id) }}" alt="{{ $weapon_form->name }}"
-                        class="w-16 h-16">
-
+        <div class="flex flex-col gap-2">
+            @foreach ($long_saber_weapon_forms as $weapon_form)
+                <div class="flex items-center justify-center ">
+                    <img src="{{ route('weapon-form-image-user', [
+                        'weapon' => $weapon_form->id,
+                        'user' => $user->id,
+                    ]) }}"
+                        alt="{{ $weapon_form->name }}" class="w-8 h-8" style="">
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
+        <div class="flex flex-col gap-2">
+            @foreach ($saberstaff_weapon_forms as $weapon_form)
+                <div class="flex items-center justify-center ">
+                    <img src="{{ route('weapon-form-image-user', [
+                        'weapon' => $weapon_form->id,
+                        'user' => $user->id,
+                    ]) }}"
+                        alt="{{ $weapon_form->name }}" class="w-8 h-8" style="">
+                </div>
+            @endforeach
+        </div>
+        <div class="flex flex-col gap-2">
+            @foreach ($dual_saber_weapon_forms as $weapon_form)
+                <div class="flex items-center justify-center ">
+                    <img src="{{ route('weapon-form-image-user', [
+                        'weapon' => $weapon_form->id,
+                        'user' => $user->id,
+                    ]) }}"
+                        alt="{{ $weapon_form->name }}" class="w-8 h-8" style="">
+                </div>
+            @endforeach
+        </div>
+
 
     </div>
 
