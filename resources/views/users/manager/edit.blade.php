993f9f9d0a209eb4314aaa084fa221c64b5fce76
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-05-05 16:03:14 +0200

    Aggiunte classi mancanti

--- resources/views/users/manager/edit.blade.php
+++ resources/views/users/manager/edit.blade.php
@@ -84,7 +84,7 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <form method="POST" action="{{ route('manager.users.update', $user->id) }}">
                     @csrf
     
--- resources/views/users/rector/edit.blade.php
+++ resources/views/users/rector/edit.blade.php
@@ -86,7 +86,7 @@
                 </div>
             </div>
 
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <form method="POST" action="{{ route('rector.users.update', $user->id) }}">
                     @csrf
     
