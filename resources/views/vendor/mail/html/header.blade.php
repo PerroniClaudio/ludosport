5e651331afdd9ba29131544729abbade544caef1
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-06-03 16:50:03 +0200

    Modificato logo nelle mail. da controllare la dimensione staging

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
             @if (trim($slot) === 'Laravel')
                 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
             @else
-                <img src="https://ludosport.test/warriors" class="logo" alt="Logo">
+                <img src={{url('warriors')}} class="logo" alt="Logo">
             @endif
         </a>
     </td>
