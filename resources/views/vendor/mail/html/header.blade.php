68935d47c75991840e12ba775f4eecb4e58fc4f4
erasmosalsanoifortech <e.salsano@ifortech.com>
2025-06-03 17:04:51 +0200

    aggiunte dimensioni logo email perchè di default erano più piccole

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -5,7 +5,7 @@
             @if (trim($slot) === 'Laravel')
                 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
             @else
-                <img src={{url('warriors')}} class="logo" alt="Logo">
+                <img src={{url('warriors')}} class="logo" alt="Logo" style="width: 100px; height: 100px; max-height: 100px; max-width: 100px;">
             @endif
         </a>
     </td>
