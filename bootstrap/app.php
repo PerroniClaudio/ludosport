29ab765f69056803d168eef8ec85c11770f96357
Claudio Perroni <c.perroni@ifortech.com>
2026-03-19 10:54:35 +0100

    switch da minorenne a maggiorenne.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,13 +33,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             EnsureMinorSwitchIsCompleted::class,
         ]);
-        // Escludi logout dal CSRF - verrà verificato manualmente nel controller
-        $middleware->validateCsrfTokens(except: [
-            'logout',
-        ]);
-        $middleware->web(append: [
-            EnsureMinorSwitchIsCompleted::class,
-        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (PostTooLargeException $exception, Request $request) {
