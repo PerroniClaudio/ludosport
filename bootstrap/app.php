3704b72ec0f0bbb9fd925a79510329b8ae6e3ce0
Erasmo <e.salsano@ifortech.com>
2026-04-09 18:16:44 +0200

    Allineato con staging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -33,6 +33,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             EnsureMinorSwitchIsCompleted::class,
         ]);
+        // Escludi logout dal CSRF - verrà verificato manualmente nel controller
+        $middleware->validateCsrfTokens(except: [
+            'logout',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (PostTooLargeException $exception, Request $request) {
